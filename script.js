3dcdaf9d31e95a9f3eb9d684b2c11bfd989263c0
eliasCanalesPineda <kenshi.ukasama12345@gmail.com>
2024-02-23 11:49:58 -0600

    Se agregaron funciones y acciones en los 3 botones

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function cambiarColorBorde() {
 
 function cambiarEstiloCSS(selector, estilo, valor) {
     const styleElement = document.createElement("style");
-    styleElement.appendChild(document.createTextNode(`${selector} { ${estilo}: ${valor}; }`));
+    styleElement.appendChild(document.createTextNode(`${selector} { ${estilo}: ${valor};}`));
     document.head.appendChild(styleElement);
 }
 
@@ -24,3 +24,32 @@ function getRandomColor() {
     }
     return color;
 }
+
+function cambiarColorBody() {
+    const body = document.querySelector('body');
+    const colores = ['red', 'green', 'blue', 'purple', 'orange'];
+    const colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
+    body.style.color = colorAleatorio;
+  }
+  
+  function CrearBorde() {
+    const div = document.querySelector('.ParrafoE');
+    div.style.border = '5px solid #ff0000';
+  }
+  
+  function CambiarImagen() {
+    const imagenActual = document.querySelector('.art2 .foto');
+    imagenActual.src = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/306_f2.png';
+    const botonImagen = document.createElement('button');
+botonImagen.textContent = 'Cambiar imagen';
+botonImagen.addEventListener('click', cambiarImagen);
+
+articulo.insertBefore(botonImagen, primerBoton);
+  }
+const section = document.querySelector('section');
+
+const newDiv = document.createElement('div');
+
+newDiv.textContent = 'Este es un nuevo contenido agregado dinámicamente';
+
+section.append(newDiv);
